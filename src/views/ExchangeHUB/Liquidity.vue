a6a3e6dc598c5548528c18cea839af8f52a8345f
谢小东 <xiexiaodong@126.com>
2021-05-27 22:16:07 +0800

    asdfas

--- src/views/ExchangeHUB/Liquidity.vue
+++ src/views/ExchangeHUB/Liquidity.vue
@@ -1,31 +1,9 @@
 <template>
   <div class="Liquidity">
     <div class="block">
-      <div class="top">
-        <div class="header">
-          <div class="title">Liquidity</div>
-          <div class="icon">
-            <div class="icon-1"></div>
-            <div class="icon-2"></div>
-          </div>
-        </div>
-        <div class="title-2">Add liquidity to receive LP tokens</div>
-        <div class="add-button">Add Liquidity</div>
-      </div>
-      <div class="bottom">
-        <div class="title">
-          <div>Your Liquidity</div>
-          <div class="icon"></div>
-        </div>
-        <div class="content">Connect to a wallet to view your liquidity.</div>
-        <div class="value">
-          <div class="value-1">Don’t see a pool you joined? Import it.</div>
-          <div class="value-2">
-            Or, if you staked your LP tokens in a farm, unstake them to see them
-            here
-          </div>
-        </div>
-      </div>
+        <router-view>
+          
+        </router-view>
     </div>
   </div>
 </template>
@@ -66,142 +44,26 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+// @import url('./liquidity/liquidity.less');
 .Liquidity {
   padding-top: 72px;
   width: 594px;
   margin: 0 auto;
   > .block {
     width: 594px;
-    height: 600px;
     margin: 0 auto;
+    margin-bottom: 100px;
     // margin: 55px 531px 185px;
     padding: 22px 0px 59px 0px;
+    padding: 22px 0 49px;
     border-radius: 8px;
-    border: solid 1px rgba(255, 242, 196, 0.31);
-    background-color: #fff;
-    .top {
-      margin-left: 40px;
-      margin-right: 40px;
-      .header {
-        display: flex;
-        justify-content: space-between;
-        .title {
-          width: 115px;
-          height: 36px;
-          margin: 0 136px 7px 0;
-          font-family: NotoSansCJKkr;
-          font-size: 24px;
-          font-weight: bold;
-          font-stretch: normal;
-          font-style: normal;
-          line-height: normal;
-          letter-spacing: normal;
-          color: #000;
-        }
-        .icon {
-          display: flex;
-          .icon-1 {
-            background-color: #f00;
-            width: 20px;
-            height: 20px;
-            margin-right: 25px;
-          }
-          .icon-2 {
-            background-color: #ff0;
-            width: 20px;
-            height: 20px;
-          }
-        }
-      }
-      .title-2 {
-        height: 24px;
-        margin: 7px 59px 47px 0;
-        font-family: NotoSansCJKkr;
-        font-size: 16px;
-        font-weight: 300;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        color: #000;
-        margin-bottom: 47px;
-      }
-      .add-button {
-        cursor: pointer;
-        width: 183px;
-        height: 58px;
-        line-height: 58px;
-        margin: 40px 10px 35.2px 40px;
-        border-radius: 6px;
-        background-image: linear-gradient(to left, #ffe505, #ffc81c 0%);
-        font-family: NotoSansCJKkr;
-        font-size: 20px;
-        font-weight: 500;
-        font-stretch: normal;
-        font-style: normal;
-        letter-spacing: normal;
-        text-align: center;
-        color: #010033;
-      }
-    }
-    .bottom {
-      border-top: solid 1px #d8d8d8;
-      .title {
-        display: flex;
-        justify-content: space-between;
-        height: 27px;
-        margin: 28.8px 61px 68px 40px;
-        font-family: NotoSansCJKkr;
-        font-size: 18px;
-        font-weight: 500;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        color: #000;
-
-        .icon {
-          width: 20px;
-          height: 20px;
-          background-color: #f00;
-        }
-      }
-      .content {
-        text-align: center;
-        margin: 68px 0px 74px 0;
-        font-family: NotoSansCJKkr;
-        font-size: 20px;
-        font-weight: 300;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        color: #000;
-      }
-      .value {
-        margin-left: 30px;
-        color: #000;
-        font-family: NotoSansCJKkr;
-        font-size: 16px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        .value-2 {
-          margin-top: 15px;
-        }
-      }
-    }
+    border: solid 1px #d8d8d8;
+    background-color: #ffffff;
   }
 }
 .el-icon-arrow-down {
   font-size: 12px;
 }
-.el-dropdown {
-  .el-icon-arrow-down {
-    font-size: 18px;
-  }
-}
+
 </style>
---
+++ src/views/ExchangeHUB/liquidity/index.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <div class="top">
+        <div class="header">
+          <div class="title">Liquidity</div>
+          <div class="icon">
+            <div class="icon-1">
+               <img  class="icon-img-20" src="../../../assets/icons-settings@2x.png">
+            </div>
+            <div class="icon-2">
+               <img class="icon-img-20" src="../../../assets/icons-recent@2x.png"/>
+            </div>
+          </div>
+        </div>
+        <div class="title-2">Add liquidity to receive LP tokens</div>
+        <div @click="addLiquidity" class="add-button">Add Liquidity</div>
+      </div>
+      <div class="bottom">
+        <div class="title">
+          <div>Your Liquidity</div>
+          <div class="icon">
+            <img  src="../../../assets/icons-help@2x.png" class="icon-img-20"/>
+          </div>
+        </div>
+        <div v-if="dataList.length==0" class="content">Connect to a wallet to view your liquidity.</div>
+        <div v-if="dataList.length!==0" class="content-list">
+              <div v-for="(item,i) in dataList" :key="i" class="content-item">
+                  <div class="content-i-head">
+                            <div>
+                                <img v-for="(img,idx) in item.imgs" :key="idx" :src="img"  class="head-img">
+                                <span class="head-title">HD/HT</span>
+                            </div>
+                            <div>
+                                <img class="list-row-down" src="../../../assets/down-icon.png"/>
+                            </div>
+                  </div>
+              </div>
+        </div>
+        <div class="value">
+          <div class="value-1">Don’t see a pool you joined? Import it.</div>
+          <div class="value-2">
+            Or, if you staked your LP tokens in a farm, unstake them to see them
+            here
+          </div>
+        </div>
+      </div>    
+    </div>
+</template>
+<style lang="less" scoped>
+@import url('./liquidity.less');
+.content-list{
+     margin: 21px 40px 17px;
+}
+.content-item{
+    padding: 24px 18px 23px 19px;
+    border-radius: 6px;
+    box-shadow: 2px 2px 4px 0 #e7e7e7;
+    border: solid 1px #d8d8d8;
+    background-color: #ffffff;
+   
+}
+.content-i-head{
+    display: flex;
+    justify-content: space-between;
+}
+.Liquidity{
+    .head-title{
+        color: #2f303f;font-size: 20px;
+    }
+    .head-img{
+        width:32px;height: 32px;
+    }
+}
+</style>
+<script>
+export default {
+       data(){
+           return {
+               dataList:[
+                   {
+                       imgs:[
+                           '../../../assets/icons-default-img-2@2x.png',
+                           '../../../assets/icons-default-img-3@2x.png'
+                       ],
+                       coinName:'HD/HT',
+                       children:[
+                           {
+                               name:'Pooled HD :',
+                               value:'3.22221',
+                               icons:['../../../assets/icons-default-img-3@2x.png'],
+                           },
+                           {
+                               name:'Pooled HT :',
+                               value:'0.34256',
+                               icons:['../../../assets/icons-default-img-2@2x.png'],
+                           },
+                           {
+                               name:'Your pool tokens:',
+                               value:'0.6134',
+                           },
+                           {
+                               name:'Your pool share:',
+                               value:'0.00%',
+                           }
+                       ]
+                   }
+               ]
+           }
+       },
+       methods:{
+           
+           addLiquidity(){
+               this.$router.push({
+                   path:'/ExchangeHUB/Liquidity/Supply-Liquidity'
+               })
+           }
+       }
+}
+</script>
